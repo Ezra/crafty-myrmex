dce42b59a61a87ba3d99d802c9fa3f404d41c3a7
Ezra Bradford <ezra.bradford@gmail.com>
2013-09-26 02:16:43 -0700

    show a single deck of cards

--- cards/src/scenes.js
+++ cards/src/scenes.js
@@ -1,5 +1,16 @@
 Crafty.scene('Game', function() {
-	Crafty.e('Card').back().at(40,40);
-	Crafty.e('Card').face("assets/1-1.jpg").at(40,59);
-	Crafty.e('Card').face("assets/1-2.jpg").at(40,78);
+	for(var rank=0; rank<=10; rank++) {
+		var max = (rank>=2 && rank<=10)?3:6
+		for(var suit=1; suit<=max; suit++) {
+			var x=40*rank;
+			var y=40*suit;
+			if(rank>=1 && rank<=10) {
+				Crafty.e('Card').at(x,y)
+					.face("assets/"+rank+"-"+suit+".jpg");
+			} else {
+				Crafty.e('Card').at(x,y)
+					.back();
+			}
+		}
+	}
 });
