11524320ef2d23d703b1fd7692c466c2766876fb
Ezra Bradford <ezra.bradford@gmail.com>
2013-09-25 20:02:33 -0700

    basic test game

---
+++ game.js
@@ -0,0 +1,48 @@
+Crafty.c('Dude', {
+	init: function() {
+		this.requires('2D, Canvas, Grid, Twoway, Gravity, Color')
+			.twoway(4)
+			.color('rgb(170,125,40)');
+	}
+}
+
+Crafty.c('Grid', {
+	init: function() {
+		this.attr({
+			w: Game.map_grid.tile.width,
+			h: Game.map_grid.tile.height
+		})
+	},
+	at: function(x,y) {
+		 if (x === undefined && y === undefined) {
+			return { x: this.x/Game.map_grid.tile.width, y: this.y/Game.map_grid.tile.height }
+		} else {
+			this.attr({ x: x * Game.map_grid.tile.width, y: y * Game.map_grid.tile.height });
+			return this;
+		}
+	}
+});
+
+Game = {
+
+	// Grid & size
+	map_grid: {
+		width: 24,
+		height: 16,
+		tile: {
+			width: 16,
+			height: 16
+		}
+	},
+	width: function() {
+		return this.map_grid.width * this.map_grid.tile.width;
+	},
+	height: function() {
+		return this.map_grid.height * this.map_grid.tile.width;
+	},
+
+	start: function() {
+		Crafty.init( Game.width(), Game.height());
+		Crafty.background('rgb(87,109,20)');
+	}
+}
