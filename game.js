dbe23d649a3b63d4e03307fbb2bb43860f2ec943
Ezra Bradford <ezra.bradford@gmail.com>
2013-09-25 22:43:13 -0700

    factor out components.js

--- game.js
+++ game.js
@@ -1,29 +1,9 @@
-Crafty.c('Dude', {
-	init: function() {
-		this.requires('2D, Canvas, Grid, Twoway, Gravity, Color')
-			.twoway(4)
-			.color('rgb(255,0,255)');
-	}
-});
-
-Crafty.c('Grid', {
-	init: function() {
-		this.attr({
-			w: Game.map_grid.tile.width,
-			h: Game.map_grid.tile.height
-		})
-	},
-	at: function(x,y) {
-		 if (x === undefined && y === undefined) {
-			return { x: this.x/Game.map_grid.tile.width, y: this.y/Game.map_grid.tile.height }
-		} else {
-			this.attr({ x: x * Game.map_grid.tile.width, y: y * Game.map_grid.tile.height });
-			return this;
-		}
-	}
-});
-
 Game = {
+	start: function() {
+		Crafty.init( Game.width(), Game.height());
+		Crafty.background('rgb(255,200,200)');
+		Crafty.e('PlayerCharacter').at(5,5);
+	},
 
 	// Grid & size
 	map_grid: {
@@ -39,11 +19,5 @@ Game = {
 	},
 	height: function() {
 		return this.map_grid.height * this.map_grid.tile.width;
-	},
-
-	start: function() {
-		Crafty.init( Game.width(), Game.height());
-		Crafty.background('rgb(255,200,200)');
-		Crafty.e('Dude').at(5,5);
 	}
 }
