b5543d8f8fc74924d0a4f01c06085ceacc9cef71
Ezra Bradford <ezra.bradford@gmail.com>
2013-09-26 01:05:15 -0700

    tidy image code

--- cards/src/components.js
+++ cards/src/components.js
@@ -1,11 +1,7 @@
 Crafty.c('Card', {
 	init: function() {
-		this.requires('2D, Canvas, Draggable, Image');
-		this.image('assets/back.png');
-		this.attr({
-			w: 130,
-			h: 182
-		})
+		this.requires('2D, Canvas, Draggable, Image')
+			.image('assets/back.png');
 	},
 	at: function(x,y) {
 		 if (x === undefined && y === undefined) {
