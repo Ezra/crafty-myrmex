e7d631d957b7a5493867508a7590a3660a336c43
Ezra Bradford <ezra.bradford@gmail.com>
2013-09-26 01:26:05 -0700

    preliminary front/back code

--- cards/src/components.js
+++ cards/src/components.js
@@ -1,8 +1,28 @@
 Crafty.c('Card', {
 	init: function() {
+		this.backImage = 'assets/back.png';
 		this.requires('2D, Canvas, Draggable, Image')
-			.image('assets/back.png');
+			.image(this.backImage);
 	},
+
+	front: function(img) {
+		if (img) {
+			this.frontImage = img;
+			this.image(this.frontImage);
+		} else if (this.frontImage) {
+			this.image(this.frontImage);
+		}
+	},
+
+	back: function(img) {
+		if (img) {
+			this.backImage = img;
+			this.image(this.frontImage);
+		} else {
+			this.image(this.backImage);
+		}
+	},
+
 	at: function(x,y) {
 		 if (x === undefined && y === undefined) {
 			return { x: this.x, y: this.y}
