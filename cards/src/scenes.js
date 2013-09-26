69b3386706e24326ea22e860450ca0ba79f59fe0
Ezra Bradford <ezra.bradford@gmail.com>
2013-09-26 14:53:48 -0700

    add blanks for foundations

--- cards/src/scenes.js
+++ cards/src/scenes.js
@@ -1,9 +1,18 @@
 Crafty.scene('Game', function() {
+	// Align
+	Crafty.viewport.scroll('_x',4);
+	Crafty.viewport.scroll('_y',184);
+
+	// Blanks
+	Crafty.e('Gridded').at(0,-10);
+	for(var x=2; x<8; x++) {
+		Crafty.e('Gridded').at(x,-10);
+	}
 	for(var x=0; x<8; x++) {
 		Crafty.e('Gridded').at(x,0);
 	}
-	Crafty.viewport.scroll('_x',4);
-	Crafty.viewport.scroll('_y',180);
+
+	// Cards
 	for(var rank=10; rank>=1; rank--) {
 		var max = (rank>1 && rank<10)?3:6
 		for(var suit=0; suit<6; suit++) {
