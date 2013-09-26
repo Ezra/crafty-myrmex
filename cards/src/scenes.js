7468e9f1557ae24b34d43f1765f3ee614aa0201a
Ezra Bradford <ezra.bradford@gmail.com>
2013-09-26 02:26:14 -0700

    show a full myrmex deck (plus six facedowns)

--- cards/src/scenes.js
+++ cards/src/scenes.js
@@ -1,12 +1,16 @@
 Crafty.scene('Game', function() {
 	for(var rank=0; rank<=10; rank++) {
-		var max = (rank>=2 && rank<=10)?3:6
-		for(var suit=1; suit<=max; suit++) {
-			var x=40*rank;
-			var y=40*suit;
-			if(rank>=1 && rank<=10) {
+		var max = (rank>1 && rank<10)?3:6
+		for(var suit=0; suit<6; suit++) {
+			var x=50+40*rank;
+			var y=50+40*suit;
+
+			var r=rank;
+			var s=(suit%max)+1;
+
+			if(r>=1 && r<=10) {
 				Crafty.e('Card').at(x,y)
-					.face("assets/"+rank+"-"+suit+".jpg");
+					.face("assets/"+r+"-"+s+".jpg");
 			} else {
 				Crafty.e('Card').at(x,y)
 					.back();
