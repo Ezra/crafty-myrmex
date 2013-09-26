f4a959669a821f1de4799a82224210fa01b14271
Ezra Bradford <ezra.bradford@gmail.com>
2013-09-26 01:53:27 -0700

    change 20-spacing to 19-spacing

--- cards/src/scenes.js
+++ cards/src/scenes.js
@@ -1,5 +1,5 @@
 Crafty.scene('Game', function() {
 	Crafty.e('Card').back().at(40,40);
-	Crafty.e('Card').face("assets/1-1.jpg").at(40,60);
-	Crafty.e('Card').face("assets/1-2.jpg").at(40,80);
+	Crafty.e('Card').face("assets/1-1.jpg").at(40,59);
+	Crafty.e('Card').face("assets/1-2.jpg").at(40,78);
 });
