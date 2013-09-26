1c167b71b1310b31a5d49b6c96444e7a0ccd729e
Ezra Bradford <ezra.bradford@gmail.com>
2013-09-26 02:30:21 -0700

    one full myrmex deck

--- cards/src/scenes.js
+++ cards/src/scenes.js
@@ -1,9 +1,9 @@
 Crafty.scene('Game', function() {
-	for(var rank=0; rank<=10; rank++) {
+	for(var rank=10; rank>=1; rank--) {
 		var max = (rank>1 && rank<10)?3:6
 		for(var suit=0; suit<6; suit++) {
-			var x=50+40*rank;
-			var y=50+40*suit;
+			var x=0+128*suit;
+			var y=0+18*(10-rank);
 
 			var r=rank;
 			var s=(suit%max)+1;
